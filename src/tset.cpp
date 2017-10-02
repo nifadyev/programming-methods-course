c4e7844d21a5ab7aa1fb423988c7ceea5191b426
Vadim Nifadyev <vnifadyev@gmail.com>
2017-10-02 17:09:24 +0000

    tset.cpp отредактирован онлайн на Bitbucket

--- src/tset.cpp
+++ src/tset.cpp
@@ -9,98 +9,80 @@
 
 TSet::TSet(int mp) : BitField(-1)
 {
-	//TODO:
 }
 
-
-TSet::TSet(const TSet &s) : BitField(-1) // конструктор копирования
+// конструктор копирования
+TSet::TSet(const TSet &s) : BitField(-1)
 {
-	//TODO:
 }
 
-
-TSet::TSet(const TBitField &bf) : BitField(-1) // конструктор преобразования типа
+// конструктор преобразования типа
+TSet::TSet(const TBitField &bf) : BitField(-1)
 {
-	//TODO:
 }
 
 TSet::operator TBitField()
 {
-	//TODO:
 }
 
 int TSet::GetMaxPower(void) const // получить макс. к-во эл-тов
 {
-	//TODO:
 }
 
 int TSet::IsMember(const int Elem) const // элемент множества?
 {
-	//TODO:
     return 0;
 }
 
 void TSet::InsElem(const int Elem) // включение элемента множества
 {
-	//TODO:
 }
 
 void TSet::DelElem(const int Elem) // исключение элемента множества
 {
-	//TODO:
 }
 
 // теоретико-множественные операции
 
 TSet& TSet::operator=(const TSet &s) // присваивание
 {
-	//TODO:
 }
 
 int TSet::operator==(const TSet &s) const // сравнение
 {
-	//TODO:
     return 0;
 }
 
 int TSet::operator!=(const TSet &s) const // сравнение
 {
-	//TODO:
 }
 
 TSet TSet::operator+(const TSet &s) // объединение
 {
-	//TODO:
 }
 
 TSet TSet::operator+(const int Elem) // объединение с элементом
 {
-	//TODO:
-	//TELEM mask = 1 << 3;
 }
 
 TSet TSet::operator-(const int Elem) // разность с элементом
 {
-	//TODO:
 }
 
 TSet TSet::operator*(const TSet &s) // пересечение
 {
-	//TODO:
 }
 
 TSet TSet::operator~(void) // дополнение
 {
-	//TODO:
 }
 
 // перегрузка ввода/вывода
+
 istream &operator>>(istream &istr, TSet &s) // ввод
 {
-	//TODO:
 }
 
 ostream& operator<<(ostream &ostr, const TSet &s) // вывод
 {
-	//TODO:
 }
