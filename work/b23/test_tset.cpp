18c0af81c82a8be17be093fedeade6865824f44e
Vadim Nifadyev <vnifadyev@gmail.com>
2017-10-05 08:13:28 +0000

    Changed names of methods

--- work/b23/test_tset.cpp
+++ work/b23/test_tset.cpp
@@ -12,7 +12,7 @@ TEST(TSet, CanInsertNonExistingElement)
 {
 	const int size = 5, k = 3;
 	TSet set(size);
-	set.InsertElem(k);
+	set.InsElem(k);
 	EXPECT_NE(set.IsMember(k), 0);
 }
 
@@ -21,8 +21,8 @@ TEST(TSet, CanInsertExistingElement)
 	const int size = 5;
 	const int k = 3;
 	TSet set(size);
-	set.InsertElem(k);
-	set.InsertElem(k);
+	set.InsElem(k);
+	set.InsElem(k);
 	EXPECT_NE(set.IsMember(k), 0);
 }
 
@@ -30,7 +30,7 @@ TEST(TSet, CanDeleteNonExistingElement)
 {
 	const int size = 5, k = 3;
 	TSet set(size);
-	set.DeleteElem(k);
+	set.DelElem(k);
 	EXPECT_EQ(set.IsMember(k), 0);
 }
 
@@ -38,9 +38,9 @@ TEST(TSet, CanDeleteExistingElement)
 {
 	const int size = 5, k = 3;
 	TSet set(size);
-	set.InsertElem(k);
+	set.InsElem(k);
 	EXPECT_GT(set.IsMember(k), 0);
-	set.DeleteElem(k);
+	set.DelElem(k);
 	EXPECT_EQ(set.IsMember(k), 0);
 }
 
@@ -56,10 +56,10 @@ TEST(TSet, CompareTwoEqualSets)
 	const int size = 4;
 	TSet set1(size), set2(size);
 	// set1 = set2 = {1, 3}
-	set1.InsertElem(1);
-	set1.InsertElem(3);
-	set2.InsertElem(1);
-	set2.InsertElem(3);
+	set1.InsElem(1);
+	set1.InsElem(3);
+	set2.InsElem(1);
+	set2.InsElem(3);
 	EXPECT_EQ(set1, set2);
 }
 
@@ -68,11 +68,11 @@ TEST(TSet, CompareTwoNonEqualSets)
 	const int size = 4;
 	TSet set1(size), set2(size);
 	// set1 = {1, 3}
-	set1.InsertElem(1);
-	set1.InsertElem(3);
+	set1.InsElem(1);
+	set1.InsElem(3);
 	// set2 = {1, 2}
-	set2.InsertElem(1);
-	set2.InsertElem(2);
+	set2.InsElem(1);
+	set2.InsElem(2);
 	EXPECT_EQ(1, set1 != set2);
 }
 
@@ -81,8 +81,8 @@ TEST(TSet, CanAssignSetOfEqualSize)
 	const int size = 4;
 	TSet set1(size), set2(size);
 	// set1 = {1, 3}
-	set1.InsertElem(1);
-	set1.InsertElem(3);
+	set1.InsElem(1);
+	set1.InsElem(3);
 	set2 = set1;
 	EXPECT_EQ(set1, set2);
 }
@@ -92,8 +92,8 @@ TEST(TSet, CanAssignSetOfGreaterSize)
 	const int size1 = 4, size2 = 6;
 	TSet set1(size1), set2(size2);
 	// set1 = {1, 3}
-	set1.InsertElem(1);
-	set1.InsertElem(3);
+	set1.InsElem(1);
+	set1.InsElem(3);
 	set2 = set1;
 	EXPECT_EQ(set1, set2);
 }
@@ -103,9 +103,9 @@ TEST(TSet, CanAssignSetOfLessSize)
 	const int size1 = 6, size2 = 4;
 	TSet set1(size1), set2(size2);
 	// set1 = {1, 3, 5}
-	set1.InsertElem(1);
-	set1.InsertElem(3);
-	set1.InsertElem(5);
+	set1.InsElem(1);
+	set1.InsElem(3);
+	set1.InsElem(5);
 	set2 = set1;
 	EXPECT_EQ(set1, set2);
 }
@@ -115,8 +115,8 @@ TEST(TSet, CanInsertNonExistingElementUsingPlusOperator)
 	const int size = 4;
 	const int k = 3;
 	TSet set(size), result(size);
-	set.InsertElem(0);
-	set.InsertElem(2);
+	set.InsElem(0);
+	set.InsElem(2);
 	result = set + k;
 	EXPECT_NE(0, result.IsMember(k));
 }
@@ -126,8 +126,8 @@ TEST(TSet, ThrowsWhenInsertNonExistingElementOutOfRangeUsingPlusOperator)
 	const int size = 4;
 	const int k = 6;
 	TSet set(size), result(size);
-	set.InsertElem(0);
-	set.InsertElem(2);
+	set.InsElem(0);
+	set.InsElem(2);
 	ASSERT_ANY_THROW(result = set + k);
 }
 
@@ -136,8 +136,8 @@ TEST(TSet, CanInsertExistingElementUsingPlusOperator)
 	const int size = 4;
 	const int k = 3;
 	TSet set(size), result(size);
-	set.InsertElem(0);
-	set.InsertElem(k);
+	set.InsElem(0);
+	set.InsElem(k);
 	result = set + k;
 	EXPECT_NE(0, set.IsMember(k));
 }
@@ -147,13 +147,13 @@ TEST(TSet, CheckSizeOfTheCombinationOfTwoSetsOfEqualSize)
 	const int size = 5;
 	TSet set1(size), set2(size), result(size);
 	// set1 = {1, 2, 4}
-	set1.InsertElem(1);
-	set1.InsertElem(2);
-	set1.InsertElem(4);
+	set1.InsElem(1);
+	set1.InsElem(2);
+	set1.InsElem(4);
 	// set2 = {0, 1, 2}
-	set2.InsertElem(0);
-	set2.InsertElem(1);
-	set2.InsertElem(2);
+	set2.InsElem(0);
+	set2.InsElem(1);
+	set2.InsElem(2);
 	result = set1 + set2;
 	EXPECT_EQ(size, result.GetMaxPower());
 }
@@ -163,19 +163,19 @@ TEST(TSet, CanCombineTwoSetsOfEqualSize)
 	const int size = 5;
 	TSet set1(size), set2(size), result(size), expectedSet(size);
 	// set1 = {1, 2, 4}
-	set1.InsertElem(1);
-	set1.InsertElem(2);
-	set1.InsertElem(4);
+	set1.InsElem(1);
+	set1.InsElem(2);
+	set1.InsElem(4);
 	// set2 = {0, 1, 2}
-	set2.InsertElem(0);
-	set2.InsertElem(1);
-	set2.InsertElem(2);
+	set2.InsElem(0);
+	set2.InsElem(1);
+	set2.InsElem(2);
 	result = set1 + set2;
 	// expectedSet = {0, 1, 2, 4}
-	expectedSet.InsertElem(0);
-	expectedSet.InsertElem(1);
-	expectedSet.InsertElem(2);
-	expectedSet.InsertElem(4);
+	expectedSet.InsElem(0);
+	expectedSet.InsElem(1);
+	expectedSet.InsElem(2);
+	expectedSet.InsElem(4);
 	EXPECT_EQ(expectedSet, result);
 }
 
@@ -184,13 +184,13 @@ TEST(TSet, CheckSizeChangesOfTheCombinationOfTwoSetsOfNonEqualSize)
 	const int size1 = 5, size2 = 7;
 	TSet set1(size1), set2(size2), result(size1);
 	// set1 = {1, 2, 4}
-	set1.InsertElem(1);
-	set1.InsertElem(2);
-	set1.InsertElem(4);
+	set1.InsElem(1);
+	set1.InsElem(2);
+	set1.InsElem(4);
 	// set2 = {0, 1, 2}
-	set2.InsertElem(0);
-	set2.InsertElem(1);
-	set2.InsertElem(2);
+	set2.InsElem(0);
+	set2.InsElem(1);
+	set2.InsElem(2);
 	result = set1 + set2;
 	EXPECT_EQ(size2, result.GetMaxPower());
 }
@@ -200,21 +200,21 @@ TEST(TSet, CanCombineTwoSetsOfNonEqualSize)
 	const int size1 = 5, size2 = 7;
 	TSet set1(size1), set2(size2), result(size1), expectedSet(size2);
 	// set1 = {1, 2, 4}
-	set1.InsertElem(1);
-	set1.InsertElem(2);
-	set1.InsertElem(4);
+	set1.InsElem(1);
+	set1.InsElem(2);
+	set1.InsElem(4);
 	// set2 = {0, 1, 2, 6}
-	set2.InsertElem(0);
-	set2.InsertElem(1);
-	set2.InsertElem(2);
-	set2.InsertElem(6);
+	set2.InsElem(0);
+	set2.InsElem(1);
+	set2.InsElem(2);
+	set2.InsElem(6);
 	result = set1 + set2;
 	// expectedSet = {0, 1, 2, 4, 6}
-	expectedSet.InsertElem(0);
-	expectedSet.InsertElem(1);
-	expectedSet.InsertElem(2);
-	expectedSet.InsertElem(4);
-	expectedSet.InsertElem(6);
+	expectedSet.InsElem(0);
+	expectedSet.InsElem(1);
+	expectedSet.InsElem(2);
+	expectedSet.InsElem(4);
+	expectedSet.InsElem(6);
 	EXPECT_EQ(expectedSet, result);
 }
 
@@ -223,17 +223,17 @@ TEST(TSet, CanIntersectTwoSetsOfEqualSize)
 	const int size = 5;
 	TSet set1(size), set2(size), result(size), expectedSet(size);
 	// set1 = {1, 2, 4}
-	set1.InsertElem(1);
-	set1.InsertElem(2);
-	set1.InsertElem(4);
+	set1.InsElem(1);
+	set1.InsElem(2);
+	set1.InsElem(4);
 	// set2 = {0, 1, 2}
-	set2.InsertElem(0);
-	set2.InsertElem(1);
-	set2.InsertElem(2);
+	set2.InsElem(0);
+	set2.InsElem(1);
+	set2.InsElem(2);
 	result = set1 * set2;
 	// expectedSet = {1, 2}
-	expectedSet.InsertElem(1);
-	expectedSet.InsertElem(2);
+	expectedSet.InsElem(1);
+	expectedSet.InsElem(2);
 	EXPECT_EQ(expectedSet, result);
 }
 
@@ -242,20 +242,20 @@ TEST(TSet, CanIntersectTwoSetsOfNonEqualSize)
 	const int size1 = 5, size2 = 7;
 	TSet set1(size1), set2(size2), result(size1), expectedSet(size2);
 	// set1 = {1, 2, 4}
-	set1.InsertElem(1);
-	set1.InsertElem(2);
-	set1.InsertElem(4);
+	set1.InsElem(1);
+	set1.InsElem(2);
+	set1.InsElem(4);
 	// set2 = {0, 1, 2, 4, 6}
-	set2.InsertElem(0);
-	set2.InsertElem(1);
-	set2.InsertElem(2);
-	set2.InsertElem(4);
-	set2.InsertElem(6);
+	set2.InsElem(0);
+	set2.InsElem(1);
+	set2.InsElem(2);
+	set2.InsElem(4);
+	set2.InsElem(6);
 	result = set1 * set2;
 	// expectedSet = {1, 2, 4}
-	expectedSet.InsertElem(1);
-	expectedSet.InsertElem(2);
-	expectedSet.InsertElem(4);
+	expectedSet.InsElem(1);
+	expectedSet.InsElem(2);
+	expectedSet.InsElem(4);
 	EXPECT_EQ(expectedSet, result);
 }
 
@@ -264,12 +264,12 @@ TEST(TSet, CheckNegationOperator)
 	const int size = 4;
 	TSet set(size), invertedSet(size), expectedSet(size);
 	// set1 = {1, 3}
-	set.InsertElem(1);
-	set.InsertElem(3);
+	set.InsElem(1);
+	set.InsElem(3);
 	invertedSet = ~set;
 	// expectedSet = {0, 2}
-	expectedSet.InsertElem(0);
-	expectedSet.InsertElem(2);
+	expectedSet.InsElem(0);
+	expectedSet.InsElem(2);
 	EXPECT_EQ(expectedSet, invertedSet);
 }
 
@@ -278,13 +278,13 @@ TEST(TSetCustom, CheckDifferenceOperator)
 	const int size = 4, elem = 2;
 	TSet set(size), result(size), expectedSet(size);
 	// set = {1, 2, 3}
-	set.InsertElem(1);
-	set.InsertElem(2);
-	set.InsertElem(3);
+	set.InsElem(1);
+	set.InsElem(2);
+	set.InsElem(3);
 	result = set - elem;
 	//expectedSet = {1, 3}
-	expectedSet.InsertElem(1);
-	expectedSet.InsertElem(3);
+	expectedSet.InsElem(1);
+	expectedSet.InsElem(3);
 	EXPECT_EQ(expectedSet, result);
 	EXPECT_EQ(0, result.IsMember(elem));		
 }
@@ -294,8 +294,8 @@ TEST(TSetCustom, ThrowsWhenSubstractTooLargeElement)
 	const int size = 4;
 	const int k = 6;
 	TSet set(size), result(size);
-	set.InsertElem(0);
-	set.InsertElem(2);
+	set.InsElem(0);
+	set.InsElem(2);
 	ASSERT_ANY_THROW(result = set - k);
 }
 
@@ -305,8 +305,8 @@ TEST(TSetCustom, CanSubtractNonExistingElement)
 	const int k = 3;
 	TSet set(size), result(size);
 	//set = {0, 2}
-	set.InsertElem(0);
-	set.InsertElem(2);
+	set.InsElem(0);
+	set.InsElem(2);
 	//result = {0, 2}
 	result = set - k;
 	EXPECT_NE(1, result.IsMember(k));
