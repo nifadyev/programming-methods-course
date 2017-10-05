f1f4b4f6c91d5b7060ea80a507f7b67de39fc365
Vadim Nifadyev <vnifadyev@gmail.com>
2017-10-05 07:59:09 +0000

    Changed names of methods

--- work/b23/tbitfield.h
+++ work/b23/tbitfield.h
@@ -27,7 +27,7 @@ public:
 	// доступ к битам
 	int GetLength(void) const;      // получить длину (к-во битов)           (#О)
 	void SetBit(const int n);      // установить бит                       (#О4)
-	void ClearBit(const int n);       // очистить бит                         (#П2)
+	void ClrBit(const int n);       // очистить бит                         (#П2)
 	int  GetBit(const int n) const; // получить значение бита               (#Л1)
 
 	// битовые операции
