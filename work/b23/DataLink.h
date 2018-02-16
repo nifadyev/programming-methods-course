02306584812148b80a893ffd3519ba3cafaebdbd
Neaph <vnifadyev@gmail.com>
2018-02-16 21:24:49 +0300

    SetDataValue: added const to parameter
    GetDataValue: return const
    
    GetNextDataLink: return nullptr or (pTDataLink)pNext
    
    TDataLink constructor: default parameters values changed to nullptr

---
+++ work/b23/DataLink.h
@@ -0,0 +1,30 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+#include "RootLink.h"
+
+#ifndef TDATALINK
+#define TDATALINK
+
+
+class TDataLink;
+typedef TDataLink *pTDataLink;
+
+class TDataLink : public TRootLink
+{
+protected:
+	pTDataValue pValue;
+public:
+	TDataLink(pTDataValue pVal = nullptr /*NULL*/, pTRootLink pNxt = nullptr /*NULL*/) : TRootLink(pNxt)
+	{
+		pValue = pVal;
+	}
+
+	void SetDataValue(const pTDataValue pVal) { pValue = pVal; }
+	pTDataValue GetDataValue() const { return pValue; }
+
+	pTDataLink GetNextDataLink() { return (pTDataLink)pNext == nullptr ? (pTDataLink)pNext : nullptr; }
+
+	friend class TDataList;
+};
+#endif
