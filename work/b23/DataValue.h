632e885a05398afe35d9ad7a0c50d3ebd75edcdd
Neaph <vnifadyev@gmail.com>
2018-02-16 21:26:12 +0300

    defined pTDataValue

---
+++ work/b23/DataValue.h
@@ -0,0 +1,20 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+#ifndef TDATAVALUE
+#define TDATAVALUE
+
+class TDataValue
+{
+public:
+	virtual TDataValue* GetCopy() const = 0;
+	~TDataValue() {}
+
+	//int operator==(const TDataValue& value)
+	//{
+	//	return *this == value;
+	//}
+};
+
+typedef TDataValue *pTDataValue;
+#endif
