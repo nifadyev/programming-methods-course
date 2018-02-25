0509e47523438d57b443102e99417676f2d32625
Neaph <vnifadyev@gmail.com>
2018-02-25 16:57:12 +0300

    Created header file for THeadRing class

---
+++ work/b23/HeadRing.h
@@ -0,0 +1,20 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+#include "DataList.h"
+
+#ifndef HEADRING
+#define HEADRING
+class THeadRing : public TDataList
+{
+protected:
+	pTDataLink pHead; // заголовок, pFirst - звено за pHead
+public:
+	THeadRing();
+	~THeadRing();
+	
+	virtual void InsertBeforeFirst(pTDataValue pVal = nullptr); // после заголовка
+
+	virtual void DeleteFirst();
+};
+#endif
