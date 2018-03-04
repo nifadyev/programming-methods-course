9332d1a510a252f53361a42e04b58b968696d501
Neaph <vnifadyev@gmail.com>
2018-03-04 20:13:09 +0300

    Implemented copy constructor and operator =
    Created draft of operator +

--- work/b23/Polinom.cpp
+++ work/b23/Polinom.cpp
@@ -6,47 +6,155 @@ TPolinom::TPolinom(int monoms[][2], int monomsNumber)/* : THeadRing()*/
 	{
 		throw invalid_argument("Error! Number of monoms must be positive");
 	}
-
+	//listLength = monomsNumber;
 	TMonom *head = new TMonom(0, -1);
-	pHead = new TDataLink(head->GetCopy());
+	pHead = new TDataLink();
+	pHead->SetDataValue(head->GetCopy());
 
-	for (int i = 0; i < monomsNumber; i++)
+	if (monoms != nullptr)
 	{
-		TMonom *monom = new TMonom(monoms[i][0], monoms[i][1]);
-		InsertAfterLast(monom->GetCopy());
+		for (int i = 0; i < monomsNumber; i++)
+		{
+			TMonom *monom = new TMonom(monoms[i][0], monoms[i][1]);
+			InsertAfterLast(monom->GetCopy());
+		}
 	}
+
+	Reset();
+	//if (monoms == nullptr)
+	//{
+	//	listLength = monomsNumber;
+	//}
 }
 
 TPolinom::TPolinom(/*const*/ TPolinom& polinom)
 {
 	if (polinom.IsEmpty())
 	{
-		//TPolinom();
-		//DeleteList();
 		return;
 	}
 
 	else
 	{
-		pHead = polinom.pHead;
+		pHead = new TDataLink();
+		pHead->SetDataValue(polinom.pHead->GetDataValue());
 
 		for (int i = 0; i < polinom.GetListLength(); i++)
 		{
-			//polinom.SetCurrentPosition(i);
+			polinom.SetCurrentPosition(i);
 			InsertAfterLast(polinom.GetDataValue(CURRENT));
-			polinom.MoveNext();
 		}
+		polinom.Reset();
 	}
 }
 
-TPolinom & TPolinom::operator+(TPolinom & polinom)
+//TPolinom::~TPolinom()
+//{
+//	DeleteList();
+//	if (pHead != nullptr)
+//	{
+//		DeleteLink(pHead);
+//	}
+//	//pHead = nullptr;
+//}
+
+TPolinom& TPolinom::operator+(TPolinom & polinom)
 {
-	// TODO: вставьте здесь оператор return
-	return *this;
+	if (polinom.IsEmpty())
+	{
+		return *this;
+	}
+
+	if (IsEmpty())
+	{
+		return polinom;
+	}
+
+	const int maxLength = GetListLength() > polinom.GetListLength() ? GetListLength() : polinom.GetListLength();
+	TPolinom *result = new TPolinom(nullptr, maxLength);
+
+	for (int i = 0; i < maxLength; i++)
+	{
+		if (IsListEnded() || polinom.IsListEnded())
+		{
+			if (IsListEnded())
+			{
+				result->InsertAfterLast(polinom.GetDataValue());
+			}
+
+			if (polinom.IsListEnded())
+			{
+				result->InsertAfterLast(GetDataValue());
+			}
+			continue;
+		}
+		//if (!IsListEnded())
+		//{
+		//	SetCurrentPosition(i);
+		//}
+		//if (!polinom.IsListEnded())
+		//{
+		//	polinom.SetCurrentPosition(i);
+		//}
+		SetCurrentPosition(i);
+		polinom.SetCurrentPosition(i);
+		TMonom *lhs = GetMonom();
+		TMonom *rhs = polinom.GetMonom();
+
+		if (lhs->GetIndex() > rhs->GetIndex())
+		{
+			result->InsertAfterLast(lhs->GetCopy());
+			continue;
+		}
+
+		else if (lhs->GetIndex() == rhs->GetIndex())
+		{
+			TMonom *temp = lhs; // Could be incorrent
+			if (lhs->GetCoefficient() != rhs->GetCoefficient())
+			{
+
+				temp->SetCoefficient(lhs->GetCoefficient() + rhs->GetCoefficient());
+				//result->InsertAfterLast(temp->GetCopy());
+			}
+			
+			if (result->GetMonom()->GetCoefficient() >= temp->GetCoefficient())
+			{
+				result->InsertAfterLast(temp->GetCopy());
+			}
+
+			else
+			{
+				result->InsertBeforeCurrent(temp->GetCopy());
+			}
+
+			continue;
+		}
+
+		else /*if (lhs->GetIndex() < rhs->GetIndex())*/
+		{
+			result->InsertAfterLast(rhs->GetCopy());
+			continue;
+		}
+	}
+
+
+	return *result;
 }
 
 TPolinom & TPolinom::operator=(TPolinom & polinom)
 {
-	// TODO: вставьте здесь оператор return
+	if (polinom.IsEmpty())
+	{
+		DeleteList();
+		Reset();
+		return *this;
+	}
+
+	DeleteList();
+	for (polinom.Reset(); !polinom.IsListEnded(); polinom.MoveNext())
+	{
+		InsertAfterLast(polinom.GetDataValue());
+	}
+
 	return *this;
 }
