ec5054a968fcfe589702db24a9eee96e63c67b01
Neaph <vnifadyev@gmail.com>
2018-03-08 20:14:46 +0300

    Initial commit

---
+++ work/b23/UserComm.h
@@ -0,0 +1,16 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+#include"Polinom.h"
+#include <regex>
+#include <string>
+
+#ifndef USER_COMM
+#define USER_COMM
+class TUserComm
+{
+public:
+	TUserComm();
+	~TUserComm();
+};
+#endif
+
