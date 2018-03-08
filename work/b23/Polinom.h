5cc992bc4d31c837fc8ac888074b4a814058423a
Neaph <vnifadyev@gmail.com>
2018-03-08 20:12:46 +0300

    Draft of Input and Print method have been created

--- work/b23/Polinom.h
+++ work/b23/Polinom.h
@@ -1,3 +1,6 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
 #include "HeadRing.h"
 #include "Monom.h"
 
@@ -6,11 +9,15 @@
 class TPolinom : public THeadRing
 {
 public:
-	TPolinom(int monoms[][2] = nullptr/*NULL*/, int monomsNumber = 0);
-	TPolinom(/*const*/ TPolinom &polinom);
+	TPolinom(int monoms[][2] = nullptr, int monomsNumber = 0);
+	TPolinom(TPolinom &polinom);
+	//~TPolinom();
 	pTMonom GetMonom() { return (pTMonom)GetDataValue(); }
 
 	TPolinom& operator+(TPolinom &polinom);
 	TPolinom& operator=(TPolinom &polinom);
+
+	TPolinom& Input();
+	void Print();
 };
 #endif
