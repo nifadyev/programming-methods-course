eb3f9d0de88d9a515515386bee224bc8f87e4043
Neaph <vnifadyev@gmail.com>
2018-03-09 17:45:09 +0300

    Input and Print have been implemented

--- work/b23/Polinom.cpp
+++ work/b23/Polinom.cpp
@@ -23,6 +23,29 @@ TPolinom::TPolinom(int monoms[][2], int monomsNumber)/* : THeadRing()*/
 	Reset();
 }
 
+//TPolinom::TPolinom(int **monoms, int monomsNumber)/* : THeadRing()*/
+//{
+//	if (monomsNumber < 0)
+//	{
+//		throw invalid_argument("Error! Number of monoms must be positive");
+//	}
+//	//listLength = monomsNumber;
+//	TMonom *head = new TMonom(0, -1);
+//	pHead = new TDataLink();
+//	pHead->SetDataValue(head->GetCopy());
+//
+//	if (monoms != nullptr)
+//	{
+//		for (int i = 0; i < monomsNumber; i++)
+//		{
+//			TMonom *monom = new TMonom(monoms[i][0], monoms[i][1]);
+//			InsertAfterLast(monom->GetCopy());
+//		}
+//	}
+//
+//	Reset();
+//}
+
 TPolinom::TPolinom(/*const*/ TPolinom& polinom)
 {
 	if (polinom.IsEmpty())
@@ -156,7 +179,14 @@ TPolinom& TPolinom::operator+(TPolinom & polinom)
 TPolinom & TPolinom::operator=(TPolinom & polinom)
 {
 	TPolinom *temp = new TPolinom(polinom);
-
+	//if (IsEmpty() && temp->IsEmpty())
+	//{
+	//	return *this;
+	//}
+	//if (IsEmpty())
+	//{
+	//	return polinom;
+	//}
 	if (temp->IsEmpty())
 	{
 		DeleteList();
@@ -172,4 +202,75 @@ TPolinom & TPolinom::operator=(TPolinom & polinom)
 	temp->DeleteList();
 
 	return *this;
+}
+
+TPolinom& TPolinom::Input()
+{
+	smatch result;
+	string input;
+	regex monom("(\\+?-?\\d+)\\*x\\^?([0-9]{1})\\*y\\^?([0-9]{1})\\*z\\^?([0-9]{1})");
+	int monoms[20][2] = { 0 };
+	int monomsNumber = 0;
+
+	cout << "\n----------------------Polinom input----------------------\n";
+	cout << "Print q to exit\n";
+	cout << "Filling polinom(max number of monoms: 20)\n";
+	cout << "Example: +|-1*x^2*y^3*z^4\n";
+	while (true)
+	{
+		cout << "Enter monom: ";
+		getline(cin, input);
+
+		if (input == "q")
+		{
+			input.clear();
+			break;
+		}
+
+		string::const_iterator begin = input.cbegin();
+		string::const_iterator end = input.cend();
+
+		for (; regex_search(begin, end, result, monom); begin = result.suffix().first)
+		{
+			//int index = atoi(result[2].str().c_str()) * 100 + atoi(result[3].str().c_str()) * 10 + atoi(result[4].str().c_str());
+			//int coefficient = atoi(result[1].str().c_str());
+			//cout << coefficient << endl;
+			//cout << index << endl;
+			monoms[monomsNumber][0] = atoi(result[1].str().c_str());
+			monoms[monomsNumber][1] = atoi(result[2].str().c_str()) * 100 + atoi(result[3].str().c_str()) * 10 + atoi(result[4].str().c_str());
+			monomsNumber++;
+		}
+
+		input.clear();
+	}
+	cout << "Filling polinom is finished\n";
+	TPolinom polinom(monoms, monomsNumber);
+	
+	*this = polinom;
+
+	return *this;
+}
+
+void TPolinom::Print()
+{
+	Reset();
+
+	for (int i = 0; !IsListEnded()/*i < GetListLength() - 1*/; i++)
+	{
+		cout << GetMonom()->GetCoefficient() << "*x^" << GetMonom()->GetIndex() / 100 << "*y^" << (GetMonom()->GetIndex() % 100) / 10 << "*z^" << GetMonom()->GetIndex() % 10;
+		MoveNext();
+		if (!IsListEnded())
+		{
+			if (GetMonom()->GetCoefficient() >= 0)
+			{
+				cout << " +";
+			}
+			else
+			{
+				cout << " ";
+			}			
+		}	
+	}
+
+	cout << endl;
 }
