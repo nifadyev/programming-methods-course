9e8c6a41276de806296dc3013e5597d47f59526f
Neaph <vnifadyev@gmail.com>
2018-03-09 17:46:24 +0300

    Regex and String libraries have been added for menthod Input

--- work/b23/Polinom.h
+++ work/b23/Polinom.h
@@ -1,6 +1,8 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
+#include <regex>
+#include <string>
 #include "HeadRing.h"
 #include "Monom.h"
 
@@ -10,6 +12,7 @@ class TPolinom : public THeadRing
 {
 public:
 	TPolinom(int monoms[][2] = nullptr, int monomsNumber = 0);
+	//TPolinom(int **monoms, int monomsNumber);
 	TPolinom(TPolinom &polinom);
 	//~TPolinom();
 	pTMonom GetMonom() { return (pTMonom)GetDataValue(); }
