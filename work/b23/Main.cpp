9d0341fdc5206e64f77e7b3405d8989e0fa27e8b
Neaph <vnifadyev@gmail.com>
2018-03-09 17:42:33 +0300

    Contains demonstration of inputting, printing and summing polinoms

--- work/b23/Main.cpp
+++ work/b23/Main.cpp
@@ -1,33 +1,20 @@
-#include <iostream>
-#include "UserComm.h"
 #include "Polinom.h"
-
 
 int main()
 { 
-	// This is draft of Input Monom
-	smatch result;
-	string input;
-	regex monom("(\\+?-?\\d+)x\\^?([0-9]{1})y\\^?([0-9]{1})z\\^?([0-9]{1})");
+	TPolinom polinom1, polinom2, sum;
 
-	cout << "Enter polinom: ";
-	getline(cin, input);
-	auto begin = input.cbegin();
-	auto end = input.cend();
+	polinom1.Input();
+	polinom2.Input();
 
-	for (; regex_search(begin, end, result, monom); begin = result.suffix().first)
-	{
-		// res[0] is monom
-		// res[1] is coeff
-		// res[2] is x_exp
-		// res[3] is y_exp
-		// res[4] is z_exp
-		//TODO: add check for wrong arguments
-		int index = atoi(result[2].str().c_str()) * 100 + atoi(result[3].str().c_str()) * 10 + atoi(result[4].str().c_str());
-		cout << index << endl;
-		cout << result[0] << endl;
-		cout << result[1] << endl;
-		//v.push_back(std::make_pair(toInt(res[1].str()), index));
-	}
+	cout << "\nFirst polinom: ";
+	polinom1.Print();
+	cout << "Second polinom: ";
+	polinom2.Print();
+
+	sum = polinom1 + polinom2;
+	cout << "Sum of polinoms: ";
+	sum.Print();
+
 	return 0;
 }
