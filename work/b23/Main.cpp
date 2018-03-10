d325b4f9bcaa9ff4191ff2a70d6604413c8c541d
Neaph <vnifadyev@gmail.com>
2018-03-10 18:05:37 +0300

    Demonstration of Calculate method has been added

--- work/b23/Main.cpp
+++ work/b23/Main.cpp
@@ -3,6 +3,7 @@
 int main()
 { 
 	TPolinom polinom1, polinom2, sum;
+	const int x = 3, y = -6, z = 2;
 
 	polinom1.Input();
 	polinom2.Input();
@@ -15,6 +16,8 @@ int main()
 	sum = polinom1 + polinom2;
 	cout << "Sum of polinoms: ";
 	sum.Print();
+
+	cout << "Sum of polinoms(if x = " << x << ", y = " << y << " and z = " << z << "): " << sum.Calculate(x, y, z) << endl;
 
 	return 0;
 }
