e6fafe94fcb72ea34bda3888dcb2cc61c9c2cb13
vadim <vnifadyev@gmail.com>
2018-05-03 20:23:40 +0300

    Added tests for classes TTable and partly for TArrayTable

--- work/b23/TableTestkit.cpp
+++ work/b23/TableTestkit.cpp
@@ -1,8 +1,9 @@
 #include "TTabRecord.h"
 #include "TArrayTable.h"
+#include "TScanTable.h"
 #include <gtest/gtest.h>
 
-//----------------Testing class TTabRecord----------------   
+//----------------Testing class TTabRecord----------------
 TEST(TabRecord, Can_Create_Default_Table_Record)
 {
     ASSERT_NO_THROW(TTabRecord tableRecord);
@@ -189,10 +190,141 @@ TEST(TabRecord, Can_Compare_Table_Record_With_Itself_Using_More)
     EXPECT_FALSE(tableRecord > tableRecord);
 }
 
-
-//----------------Testing class TArrayTable---------------- 
-TEST(DISABLED_ArrayTable, Can_Create_Default_Array_Table)
+//----------------Testing class TTable----------------
+TEST(TTable, Can_Get_Data_Count)
 {
-    //TODO: TArrayTable is the parent of TScanTable
-    ASSERT_NO_THROW(TArrayTable arrayTable);
+    TScanTable table;
+
+    ASSERT_NO_THROW(table.GetDataCount());
+}
+
+TEST(TTable, Get_Data_Count_Returns_Correct_Value)
+{
+    TScanTable table(3);
+    TTabRecord record("example");
+
+    table.InsertRecord(record.GetKey(), record.GetCopy());
+
+    EXPECT_EQ(table.GetDataCount(), 1);
+}
+
+TEST(TTable, Can_Get_Efficiency)
+{
+    TScanTable table;
+
+    ASSERT_NO_THROW(table.GetEfficiency());
+}
+
+TEST(TTable, Get_Efficiency_Returns_Correct_Value)
+{
+    TScanTable table(7);
+
+    EXPECT_EQ(table.GetEfficiency(), 0);
+}
+
+//----------------Testing class TArrayTable----------------
+// TEST(ArrayTable, Can_Create_Default_Array_Table)
+// {
+//     //TODO: TArrayTable is the parent of TScanTable
+//     ASSERT_NO_THROW(TArrayTable arrayTable);
+// }
+
+TEST(ArrayTable, Is_Full_Returns_True)
+{
+    TScanTable scanTable(2);
+    TTabRecord record1("qwerty"), record2("wasd");
+
+    scanTable.InsertRecord(record1.GetKey(), record1.GetCopy());
+    scanTable.InsertRecord(record2.GetKey(), record2.GetCopy());
+
+    EXPECT_TRUE(scanTable.IsFull());
+}
+
+TEST(ArrayTable, Is_Full_Returns_False)
+{
+    TScanTable scanTable;
+
+    EXPECT_FALSE(scanTable.IsFull());
+}
+
+TEST(ArrayTable, Can_Get_Table_Size)
+{
+    TScanTable scanTable;
+
+    ASSERT_NO_THROW(scanTable.GetTableSize());
+}
+
+TEST(ArrayTable, Get_Table_Size_Returns_Correct_Value)
+{
+    TScanTable scanTable(12);
+
+    EXPECT_EQ(scanTable.GetTableSize(), 12);
+}
+
+TEST(ArrayTable, DISABLED_Can_Get_Key)
+{
+    TScanTable scanTable;
+
+    ASSERT_NO_THROW(scanTable.GetKey());
+}
+
+TEST(ArrayTable, Throw_When_Trying_To_Get_Key_From_Empty_Table)
+{
+    TScanTable scanTable;
+
+    ASSERT_THROW(scanTable.GetKey(), runtime_error);
+}
+
+TEST(ArrayTable, Throw_When_Trying_To_Get_Key_Using_Invalid_Position)
+{
+    TScanTable scanTable(5);
+    TTabRecord record("qwerty");
+
+    scanTable.InsertRecord(record.GetKey(), record.GetCopy());
+
+    ASSERT_THROW(scanTable.GetKey(), invalid_argument);
+}
+
+TEST(ArrayTable, DISABLED_Get_Key_Returns_Correct_Value)
+{
+    TScanTable scanTable(5);
+    TTabRecord record("qwerty");
+
+    scanTable.InsertRecord(record.GetKey(), record.GetCopy());
+
+    EXPECT_EQ(scanTable.GetKey(), "qwerty");
+}
+
+TEST(ArrayTable, DISABLED_Can_Get_Value)
+{
+    TScanTable scanTable;
+
+    ASSERT_NO_THROW(scanTable.GetKey());
+}
+
+TEST(ArrayTable, Throw_When_Trying_To_Get_Value_From_Empty_Table)
+{
+    TScanTable scanTable;
+
+    ASSERT_THROW(scanTable.GetValuePTR(), runtime_error);
+}
+
+TEST(ArrayTable, Throw_When_Trying_To_Get_Value_Using_Invalid_Position)
+{
+    TScanTable scanTable(5);
+    TTabRecord record("qwerty");
+
+    scanTable.InsertRecord(record.GetKey(), record.GetCopy());
+
+    ASSERT_THROW(scanTable.GetValuePTR(), invalid_argument);
+}
+
+TEST(ArrayTable, DISABLED_Get_Value_Returns_Correct_Value)
+{
+    TScanTable scanTable(5);
+    TTabRecord record("qwerty");
+
+    scanTable.InsertRecord(record.GetKey(), record.GetCopy());
+
+    EXPECT_EQ(scanTable.GetValuePTR(), nullptr);
 }
