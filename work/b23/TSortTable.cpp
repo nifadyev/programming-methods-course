184422447b7dadebb93622d17f7de1b001beb6a4
vadim <vnifadyev@gmail.com>
2018-05-05 18:49:23 +0300

    Fully implemented

--- work/b23/TSortTable.cpp
+++ work/b23/TSortTable.cpp
@@ -4,17 +4,17 @@ void TSortTable::SortData(void)
 {
     switch (sortMethod)
     {
-        case INSERT_SORT:
-            InsertSort(pRecords, dataCount);
-            break;
-        case MERGE_SORT:
-            MergeSort(pRecords, dataCount);
-            break;
-        case QUICK_SORT:
-            QuickSort(pRecords, dataCount);
-            break;
-        default:
-            throw runtime_error("Error! Sort method is not mentioned");
+    case INSERT_SORT:
+        InsertSort(pRecords, dataCount);
+        break;
+    case MERGE_SORT:
+        MergeSort(pRecords, dataCount);
+        break;
+    case QUICK_SORT:
+        QuickSort(pRecords, 0, dataCount - 1);
+        break;
+    default:
+        throw runtime_error("Error! Sort method is not mentioned");
     }
 }
 
@@ -40,7 +40,7 @@ void TSortTable::MergeSort(pTTabRecord *pMemory, int dataCount)
     MergeSorter(pRecords, 0, dataCount - 1);
 }
 
-void TSortTable::MergeSorter(pTTabRecord *& pData, int left, int right)
+void TSortTable::MergeSorter(pTTabRecord *&pData, int left, int right)
 {
     if (left == right)
     {
@@ -53,7 +53,7 @@ void TSortTable::MergeSorter(pTTabRecord *& pData, int left, int right)
     MergeData(pData, left, middle, right);
 }
 
-void TSortTable::MergeData(pTTabRecord *& pData,int left,int middle, int right)
+void TSortTable::MergeData(pTTabRecord *&pData, int left, int middle, int right)
 {
     pTTabRecord *pBuffer = new pTTabRecord[right - left + 1];
     int i = left, j = middle + 1, k = 0;
@@ -94,7 +94,7 @@ void TSortTable::MergeData(pTTabRecord *& pData,int left,int middle, int right)
     delete[] pBuffer;
 }
 
-void TSortTable::QuickSort(pTTabRecord * pMemory, int left, int right)
+void TSortTable::QuickSort(pTTabRecord *pMemory, int left, int right)
 {
     if (left < right)
     {
@@ -104,7 +104,7 @@ void TSortTable::QuickSort(pTTabRecord * pMemory, int left, int right)
     }
 }
 
-int TSortTable::QuickSplit(pTTabRecord * pData, int left, int right)
+int TSortTable::QuickSplit(pTTabRecord *pData, int left, int right)
 {
     TKey pivot = pRecords[(left + right) / 2]->GetKey();
     int i = left, j = right;
@@ -145,29 +145,165 @@ TSortTable::TSortTable(const TScanTable &table) : TScanTable(table.GetTableSize(
     Reset();
 }
 
-TSortTable & TSortTable::operator=(const TScanTable & table)
+TSortTable &TSortTable::operator=(const TScanTable &table)
 {
-    // TODO: �������� ����� �������� return
+    if (this != &table)
+    {
+        if (pRecords != nullptr)
+        {
+            for (int i = 0; i < maxTableSize; ++i)
+            {
+                if (pRecords[i] != nullptr)
+                {
+                    if (pRecords[i]->pValue != nullptr)
+                    {
+                        delete pRecords[i]->pValue;
+                    }
+                    delete pRecords[i];
+                }
+            }
+
+            delete[] pRecords;
+        }
+
+        maxTableSize = table.maxTableSize;
+        dataCount = table.dataCount;
+        pRecords = new pTTabRecord[maxTableSize];
+        for (int i = 0; i < maxTableSize; ++i)
+        {
+            if (table.pRecords[i] != nullptr)
+            // Could be incorrect
+            {
+                pRecords[i] = new TTabRecord(table.pRecords[i]->GetKey(), table.pRecords[i]->GetValuePTR());
+            }
+            else
+            {
+                pRecords[i] = nullptr;
+            }
+        }
+
+        SortData();
+        Reset();
+    }
+
+    return *this;
 }
 
 TSortMethod TSortTable::GetSortMethod(void)
 {
-    return TSortMethod();
+    return sortMethod;
 }
 
 void TSortTable::SetSortMethod(TSortMethod srtMethod)
 {
+    if (srtMethod != INSERT_SORT && srtMethod != MERGE_SORT && srtMethod != QUICK_SORT)
+    {
+        throw invalid_argument("Error! Chosed sort method is not supported");
+    }
+
+    sortMethod = srtMethod;
 }
 
 pTDataValue TSortTable::FindRecord(TKey key)
 {
-    return pTDataValue();
+    if (IsEmpty())
+    {
+        throw logic_error("Error! Cannot find record in empty table");
+    }
+    if (key < pRecords[0]->GetKey() || key > pRecords[dataCount - 1]->GetKey())
+    {
+        // Is it really needed?
+        if (key > pRecords[dataCount - 1]->GetKey())
+        {
+            currentPosition = dataCount;
+        }
+        throw runtime_error("Error! Required record is out of range");
+    }
+
+    Reset();
+    efficiency = 0;
+    int left = 0, right = dataCount;
+
+    // Try to use for instead of while
+    while ((right - left) > 0)
+    {
+        efficiency++;
+        int middle = left + (right - left) / 2;
+        if (pRecords[middle]->GetKey() == key)
+        {
+            currentPosition = middle;
+            return pRecords[middle]->GetValuePTR();
+        }
+
+        if (pRecords[middle]->GetKey() > key)
+        {
+            right = middle;
+        }
+        else
+        {
+            left = middle + 1;
+        }
+    }
+
+    currentPosition = right; // or currentPosition = left;
+
+    throw runtime_error("Error! There is no record with such key");
 }
 
 void TSortTable::InsertRecord(TKey key, pTDataValue value)
 {
+    if (IsFull())
+    {
+        throw logic_error("Error! Cannot insert record into full table");
+    }
+
+    try
+    {
+        FindRecord(key);
+    }
+    catch (...)
+    {
+        for (int i = dataCount; i > currentPosition; i--)
+        {
+            pRecords[i] = pRecords[i - 1];
+        }
+        pRecords[currentPosition] = new TTabRecord(key, value);
+        dataCount++;
+        return;
+    }
+
+    throw runtime_error("Error! Record with such data has already existed");
 }
 
 void TSortTable::DeleteRecord(TKey key)
 {
+    if (IsEmpty())
+    {
+        throw logic_error("Error! Cannot delete record from empty table");
+    }
+
+    // Nonexistent key
+    try
+    {
+        FindRecord(key);
+    }
+    catch (...)
+    {
+        throw runtime_error("Error! Record with requested key is not found");
+    }
+
+    if (pRecords[currentPosition]->pValue != nullptr)
+    {
+        delete pRecords[currentPosition]->pValue;
+    }
+    delete pRecords[currentPosition];
+    pRecords[currentPosition] = nullptr;
+
+    for (int i = currentPosition; i < dataCount - 1; ++i)
+    {
+        pRecords[i] = pRecords[i + 1];
+    }
+    pRecords[dataCount - 1] = nullptr;
+    dataCount--;
+    Reset();
 }
