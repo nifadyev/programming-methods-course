43edd2d92ce8c433be49c428b0a771995f527c22
vadim <vnifadyev@gmail.com>
2018-05-05 10:18:57 +0300

    Changed parameters in sorting methods

--- work/b23/TSortTable.h
+++ work/b23/TSortTable.h
@@ -3,32 +3,32 @@
 #ifndef TSORT_TABLE_INCLUDE_TSORT_TABLE_H
 #define TSORT_TABLE_INCLUDE_TSORT_TABLE_H
 
-enum TSortMethod { INSERT_SORT, MERGE_SORT, QUIQ_SORT };
+enum TSortMethod { INSERT_SORT, MERGE_SORT, QUICK_SORT };
 
 class  TSortTable : public TScanTable 
 {
 protected:
-    TSortMethod sortMethod; // Метод сортировки
+    TSortMethod sortMethod; // пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
 
-    void SortData(void);   // Сортировка данных
-    void InsertSort(pTTabRecord *pMemory, int dataCount); // Метод вставок
+    void SortData(void);   // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
+    void InsertSort(pTTabRecord *pMemory, int dataCount); // пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
 
-    void MergeSort(pTTabRecord *pMemory, int dataCount);  // Метод слияния
-    void MergeSorter(pTTabRecord * &pData, pTTabRecord * &pBuffer, int size);
-    void MergeData(pTTabRecord *&pData, pTTabRecord *&pBuffer, int n1, int n2); //TODO: rename args
+    void MergeSort(pTTabRecord *pMemory, int dataCount);  // пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+    void MergeSorter(pTTabRecord * &pData, int left, int right);
+    void MergeData(pTTabRecord *&pData,int left, int middle, int right); //TODO: rename args
 
-    void QuickSort(pTTabRecord *pMemory, int dataCount); // Быстрая сортировка
-    void QuickSplit(pTTabRecord *pData, int size, int &Pivot); //TODO: What is pivot?
+    void QuickSort(pTTabRecord *pMemory, int left, int right); // пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+    int QuickSplit(pTTabRecord *pData, int left, int right); //TODO: What is pivot?
 public:
-    TSortTable(int size = TABLE_MAX_SIZE) : TScanTable(size) {};
-    TSortTable(const TScanTable &table); // Из просматриваемой таблицы
+    TSortTable(int size = TABLE_MAX_SIZE) : TScanTable(size) {sortMethod = INSERT_SORT;}
+    TSortTable(const TScanTable &table); // пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
 
     TSortTable & operator=(const TScanTable &table);
 
-    TSortMethod GetSortMethod(void);    // Получить метод сортировки
-    void SetSortMethod(TSortMethod srtMethod);// Установить метод сортировки
+    TSortMethod GetSortMethod(void);    // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+    void SetSortMethod(TSortMethod srtMethod);// пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
 
-    //-----------------------------Основные методы-----------------------------
+    //-----------------------------пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ-----------------------------
     virtual pTDataValue FindRecord(TKey key);
     virtual void InsertRecord(TKey key, pTDataValue value);
     virtual void DeleteRecord(TKey key);
