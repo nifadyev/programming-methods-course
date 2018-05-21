8bd9f7029581b6863499da1a6480e3dfb4fc4328
vadim <vnifadyev@gmail.com>
2018-05-21 16:27:06 +0300

    Renamed

---
+++ work/b23/DatValue.h
@@ -0,0 +1,13 @@
+#ifndef TDAT_VALUE_INCLUDE_TDAT_VALUE_H
+#define TDAT_VALUE_INCLUDE_TDAT_VALUE_H
+
+class TDataValue
+{
+ public:
+    ~TDataValue() {}
+    virtual TDataValue* GetCopy() = 0;
+    //~TDataValue() = default;
+};
+typedef TDataValue *pTDataValue;
+
+#endif  // TDAT_VALUE_INCLUDE_TDAT_VALUE_H
